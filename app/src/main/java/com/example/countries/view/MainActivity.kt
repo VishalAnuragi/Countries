fd172a3d8162dc597748dcde50aaf11d4393d206
vishal <vishalanuragi123@gmail.com>
2022-05-11 23:14:23 +0530

    List of Countries with MVVM architecture used Retrofit and Glide

---
+++ app/src/main/java/com/example/countries/view/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.countries
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.countries.view.CountryListAdapter
+import com.example.countries.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: ListViewModel
+    private val countriesAdapter = CountryListAdapter(arrayListOf())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this)[ListViewModel::class.java]
+        viewModel.refresh()
+
+        countriesList.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = countriesAdapter
+        }
+
+        swipeRefreshLayout.setOnRefreshListener {
+            swipeRefreshLayout.isRefreshing = false
+            viewModel.refresh()
+        }
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.countries.observe(this, Observer { countries ->
+            countries?.let {
+                countriesList.visibility = View.VISIBLE
+                countriesAdapter.updateCountries(it) }
+        })
+
+        viewModel.countryLoadError.observe(this, Observer { isError ->
+            isError?.let { list_error.visibility = if (it) View.VISIBLE else View.GONE }
+        })
+
+        viewModel.loading.observe( this, Observer { isLoading ->
+            isLoading?.let {
+                loading_view.visibility = if (it) View.VISIBLE else View.GONE
+                if (it) {
+                    list_error.visibility = View.GONE
+                    countriesList.visibility = View.GONE
+                }
+            }
+        })
+    }
+}
